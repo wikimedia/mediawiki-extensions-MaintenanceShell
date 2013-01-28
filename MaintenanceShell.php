b55e5749c86f870d5f7eec342f19527651607a88
Andrew Fitzgerald <contact@swiftlytilting.com>
2013-01-27 19:23:42 -0800

    Import MaintenanceShell v0.2.0
    Imported from http://swiftlytilting.com/downloads/MaintenanceShell.0.2.0.zip
    
    Originally released at 30 May 2009 13:29 (UTC)

--- MaintenanceShell.php
+++ MaintenanceShell.php
@@ -1,6 +1,5 @@
 <?php
 
-session_start();
 # Alert the user that this is not a valid entry point to MediaWiki if they try to access the special pages file directly.
 if (!defined('MEDIAWIKI')) {
         echo <<<EOT
@@ -16,7 +15,7 @@ $wgExtensionCredits['specialpage'][] = array(
 	'url' => 'http://www.mediawiki.org/wiki/Extension:MaintenanceShell',
 	'description' => 'Adds a special page to provide access to maintenance scripts',
 	'descriptionmsg' => 'maintenanceshell-desc',
-	'version' => '0.1.0',
+	'version' => '0.2.0',
 );
  
 $dir = dirname(__FILE__) . '/';
@@ -32,64 +31,65 @@ $wgSpecialPageGroups['MaintenanceShell'] = 'wiki';
 // New user right - required to access Special:MaintenanceShell
 $wgAvailableRights[] = 'maintenanceshell';
 		
-		// catch operations before wiki does anything, so we can act like we're coming from the command line
-		
-		if (array_key_exists('commandline', $_REQUEST) && array_key_exists('title', $_REQUEST) && ($_REQUEST['title'] = 'Special:MaintenanceShell'))
-		{	$head_redirect = 'Location: ' . $_SERVER['SERVER_NAME'] . $_SERVER['SCRIPT_NAME'] . '?title=Special:MaintenanceShell';
-						
-			if ($_REQUEST['commandline'] == 'set_password')
-			{	file_put_contents($IP.'/extensions/MaintenanceShell/.password', md5($_REQUEST['pword']));
-				header($head_redirect);
+// catch operations before wiki does anything, so we can act like we're coming from the command line
+
+if (array_key_exists('commandline', $_REQUEST) && array_key_exists('title', $_REQUEST) && ($_REQUEST['title'] = 'Special:MaintenanceShell'))
+{  
+	// set up system to verify user permissions						
+	require_once('./includes/Setup.php');			
+	
+	$head_redirect = 'Location: ' . $_SERVER['SERVER_NAME'] . $_SERVER['SCRIPT_NAME'] . '?title=Special:MaintenanceShell';
+	if ( $wgUser->isBlocked() || wfReadOnly() || !$wgUser->isAllowed( 'maintenanceshell' ) ) {
+		header($head_redirect);
+		return;
+	}
+
+				
+	echo '<a href="' . $_SERVER['SCRIPT_NAME'] . '&title=Special:MaintenanceShell">Return to the Maintenance Shell</a>';
+	if (array_key_exists('script', $_REQUEST) && (trim($_REQUEST['script'] == ''))) 
+	{	echo ' | <a href="' . $_SERVER['SCRIPT_NAME'] . '?title=Special:MaintenanceShell&set_password=true">Change shell password</a>';
+	}
+	
+	$maintenance_path = $IP . "/maintenance/";
+	
+	
+	$_REQUEST['commandline'] = str_ireplace('{{root}}', $_SERVER['DOCUMENT_ROOT'], $_REQUEST['commandline']);	
+	
+	// make commandLine.inc think we're coming from the command line
+	unset($_SERVER['REQUEST_METHOD']);
+	$argv = explode(' ','0 '. $_REQUEST['commandline']);			
+	$argc = count($argv);
+	
+	echo '<hr />';
+	if (array_key_exists('script', $_REQUEST) and (($script = trim($_REQUEST['script'])) !== ''))
+	{	
+		$script = str_replace(array('.','/'), '', $script);
+			
+		$argc = count ($argv);
+		if (file_exists(trim($maintenance_path.$script.".php")))
+		{	echo "<div style='border:5px solid gray;background-color:black;color:green;padding:1em'>";
+			echo "<pre style='white-space: -moz-pre-wrap;white-space: -pre-wrap;white-space: -o-pre-wrap;white-space: pre-wrap;word-wrap: break-word;'><b>";
+			chdir(isset($wgMaintenanceShellDir) ? $wgMaintenanceShellDir : $maintenance_path);
+			echo getcwd() . "$ php ". (isset($wgMaintenanceShellDir) ? $maintenance_path : '') .$script . ".php " . $_REQUEST['commandline'] . "</b>\n\n";
+			
+			function exit_callback($param = false)
+			{							
+				echo "</pre></div><hr />$param";
 				exit;
 			}
+			register_shutdown_function('exit_callback');
 			
-			if (!file_exists($IP.'/extensions/MaintenanceShell/.password') || !array_key_exists('pword',$_REQUEST) ||
-			md5($_REQUEST['pword']) != file_get_contents($IP.'/extensions/MaintenanceShell/.password'))			 
-			{					
-				header($head_redirect.'&badpass=true&script=' . (array_key_exists('script', $_REQUEST) ? $_REQUEST['script'] :'none'));
-				exit;
-			}
+			// call the script			
+			include_once($maintenance_path.$script.".php");
 			
-			if (array_key_exists('pword',$_REQUEST))
-			{	$_SESSION['MaintenanceShell'] = $_REQUEST['pword'];
-			}
-			 
-			 
-			echo '<a href="' . $_SERVER['SCRIPT_NAME'] . '&title=Special:MaintenanceShell">Return to the Maintenance Shell</a>';
-			if (array_key_exists('script', $_REQUEST) && (trim($_REQUEST['script'] == ''))) 
-			{	echo ' | <a href="' . $_SERVER['SCRIPT_NAME'] . '?title=Special:MaintenanceShell&set_password=true">Change shell password</a>';
-			}
-			$maintenance_path = $IP . "/maintenance/";
-			
-		
-			$_REQUEST['commandline'] = str_replace('{{root}}', $_SERVER['DOCUMENT_ROOT'], $_REQUEST['commandline']);	
-			
-			// make commandLine.inc think we're coming from the command line
-			unset($_SERVER['REQUEST_METHOD']);
-			$args = explode(' ',$_REQUEST['commandline']);
-			$argv = array(0=>'');
-			$argv = array_merge($argv,$args);
-			$argc = count($argv);
-			
-			if (array_key_exists('script', $_REQUEST) and (($script = trim($_REQUEST['script'])) !== ''))
-			{	
-				$script = str_replace(array('.','/'), '', $script);
-					
-				$argc = count ($argv);
-				if (file_exists(trim($maintenance_path.$script.".php")))
-				{	echo "<pre><hr />$ php ".$script . ".php " . implode($argv, ' ') . "\n\n";
-					
-								
-					include_once($maintenance_path.$script.".php");
-					
-					echo "<hr /></pre>";
-					exit;
-				}
-				else
-				{	echo "Script '".$maintenance_path .$script."' does not exist!";
-					
-				}
-			} 
 			exit;
 		}
+		else
+		{	echo "Script '".$maintenance_path .$script."' does not exist!";
+		}
+	} 
+	exit;
+}
+
+$wgMaintenanceShellDir  = isset($wgMaintenanceShellDir) ? $wgMaintenanceShellDir :$maintenance_path;
 ?>
--- MaintenanceShell_body.php
+++ MaintenanceShell_body.php
@@ -21,53 +21,25 @@ class MaintenanceShell extends SpecialPage {
 			return;
 		}
 
-		# If the user doesn't have the required 'maintenance' permission, display an error
+		# If the user doesn't have the required 'maintenanceshell' permission, display an error
 		if( !$wgUser->isAllowed( 'maintenanceshell' )) {
 			$wgOut->permissionRequired( 'maintenanceshell' );
 			return;
 		}
-		
-
-
  
 		$this->setHeaders();
- 
-		# Get request data from, e.g.
-		
- 		if (!file_exists($IP.'/extensions/MaintenanceShell/.password') || array_key_exists('set_password',$_REQUEST))
- 		{	
- 			$str = "<b>Please set a password for the Maintenance Shell before continuing.</b>" .
- 			"<form action='$_SERVER[SCRIPT_NAME]' method='get'>" .
-			"Password <input name='pword' value='' type='password'/><br />" .
-			'<input name="title" value="Special:MaintenanceShell" type="hidden">' .
-			'<input name="commandline" type="hidden"  value="set_password"/>' .
-			'<input name="submit" type="submit" value="Set up password"/></form><hr />';
-			
-			$wgOut->addHTML($str);
-			return;
-		}
-			
-		
+ 		
  		$maintenance_path = $IP.'/maintenance/';
 
 		$str = "<b>Warning: Use these scripts with care.  They are intended for administrators and other advanced users only.</b><br /><br /><form action='$_SERVER[SCRIPT_NAME]' method='get'>" .
 			'<input name="title" value="Special:MaintenanceShell" type="hidden">' .
 			"<table><tr><td><b>Script name</b>:</td><td> <input name='script' value='" . (array_key_exists('script', $_REQUEST) ? trim($_REQUEST['script']) : '').  "'/>.php</td></tr>" .			
-			'<tr><td><b>Command line options</b>:</td><td> <input name="commandline" size="70" value="' .  (array_key_exists('commandline', $_REQUEST) ? trim($_REQUEST['commandline']) : '') .'"/></td></tr>' .
-			'<tr><td><b>Password</b>:</td><td> <input name="pword" type="password" value="'. (array_key_exists('MaintenanceShell', $_SESSION) ? $_SESSION['MaintenanceShell'] : '') . '"></td></tr></table><br /><input name="submit" type="submit" value="Run script"/></form><br /><hr />';
+			'<tr><td><b>Command line options</b>:</td><td> <input name="commandline" size="70" value="' .  (array_key_exists('commandline', $_REQUEST) ? trim($_REQUEST['commandline']) : '') .'"/></td></tr>' .			
+			'</table><br /><input name="submit" type="submit" value="Run script"/></form><br /><hr />';
 		
-		if (array_key_exists('badpass',$_REQUEST))
-		{	$str = '<span style="color:red;font-weight:bold">Invalid password used when accessing shell</span><br />' . $str;
-		}
-		
+			
 		$wgOut->addHTML($str);
-		
-		
-		
-		
-		
-		
-		
+						
 		$files = array_diff( scandir($maintenance_path ), Array( ".", ".." ) );
 		if (count($files))
 		{	$wgOut->addHTML("<b>Available maintenance scripts:</b><pre><table cellpadding=5><tr>");
@@ -86,7 +58,6 @@ class MaintenanceShell extends SpecialPage {
 			$wgOut->addHTML("</tr></table>");
 			
 		}	 
-		
 		
 		$wgOut->addHTML('</pre>');
 		
