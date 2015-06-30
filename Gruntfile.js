f3c5cda5fd5cb0c8f32e20271cca1a8927cdfd0f
paladox <thomasmulhall410@yahoo.com>
2015-06-30 21:23:19 +0200

    build: Switch test to use Grunt to include jsonlint/banana
    Change-Id: Ic343ddec88f7f229463eeb990e16aafedbf67ce8

---
+++ Gruntfile.js
@@ -0,0 +1,31 @@
+/*jshint node:true */
+module.exports = function ( grunt ) {
+	grunt.loadNpmTasks( 'grunt-banana-checker' );
+	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
+	grunt.loadNpmTasks( 'grunt-jscs' );
+	grunt.loadNpmTasks( 'grunt-jsonlint' );
+
+	grunt.initConfig( {
+		jshint: {
+			options: {
+				jshintrc: true
+			},
+			all: '.'
+		},
+		jscs: {
+			src: '.'
+		},
+		jsonlint: {
+			all: [
+				'**/*.json',
+				'!node_modules/**'
+			]
+		},
+		banana: {
+			all: 'i18n/'
+		}
+	} );
+
+	grunt.registerTask( 'test', [ 'jshint', 'jscs', 'jsonlint', 'banana' ] );
+	grunt.registerTask( 'default', 'test' );
+};
